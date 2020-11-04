024b435fff8ffc6112963b94fe518bc59ff584c6
Nick Kooman <nickjkooman@gmail.com>
2020-11-04 01:08:20 -0500

    Added repo path to public path.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/the-showcase/' : '/',
   configureWebpack: {
     resolve: {
       alias: {
